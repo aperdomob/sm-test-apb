233db920f652da5353c84d3017cbefddeec9e306
Alejandro Perdomo <aperdomobo@gmail.com>
2020-01-17 10:37:28 -0500

    feat(calc): add module operation

--- calc.js
+++ calc.js
@@ -2,5 +2,6 @@ module.exports = {
   sum = (a, b) => a + b,
   substract = (a, b) => a - b,
   multiply = (a, b) => a * b,
-  division = (a, b) => a / b
+  division = (a, b) => a / b,
+  mod = (a, b) => a % b
 };
